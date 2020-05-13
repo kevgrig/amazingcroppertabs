18262f5b3aa695beefc86e6a5c36ba184a3a651c
Kevin G <kevin@myplaceonline.com>
2020-05-12 18:34:23 -0700

    Create tabs and stacks

--- App.js
+++ App.js
@@ -1,19 +1,90 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { FontAwesome } from '@expo/vector-icons';
+
+class Tab1Screen1Component extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Tab1 Screen1</Text>
+        <Button title="Navigate to Tab1Screen2" onPress={() => { this.props.navigation.navigate("Tab1Screen2"); }} />
+      </View>
+    );
+  }
+}
+
+class Tab1Screen2Component extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Tab1 Screen2</Text>
+      </View>
+    );
+  }
+}
+
+const Tab1Stack = createStackNavigator();
+
+class Tab1StackComponent extends React.Component {
+  render() {
+    return (
+      <Tab1Stack.Navigator>
+        <Tab1Stack.Screen
+          name="Tab1Screen1"
+          component={Tab1Screen1Component}
+        />
+        <Tab1Stack.Screen name="Tab1Screen2" component={Tab1Screen2Component} />
+      </Tab1Stack.Navigator>
+    );
+  }
+}
+
+class Tab2Component extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Tab2</Text>
+      </View>
+    );
+  }
+}
+
+const Tabs = createBottomTabNavigator();
+
+class TabsComponent extends React.Component {
+  render() {
+    return (
+      <Tabs.Navigator>
+        <Tabs.Screen
+          name="Tab1"
+          component={Tab1StackComponent}
+          options={{
+            tabBarIcon: ({focused, color, size}) => {
+              return <FontAwesome name="star-o" size={size} color={color} />;
+            }
+          }}
+        />
+        <Tabs.Screen
+          name="Tab2"
+          component={Tab2Component}
+          options={{
+            tabBarIcon: ({focused, color, size}) => {
+              return <FontAwesome name="adjust" size={size} color={color} />;
+            }
+          }}
+        />
+      </Tabs.Navigator>
+    );
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <TabsComponent />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
