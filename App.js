01ebafcc899e3641ad39898a3b1c11353d4b765a
Kevin G <kevin@myplaceonline.com>
2020-05-12 18:57:29 -0700

    Produce issue with tab bar cropping

--- App.js
+++ App.js
@@ -4,52 +4,49 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { FontAwesome } from '@expo/vector-icons';
+import AmazingCropper from 'react-native-amazing-cropper';
 
-class Tab1Screen1Component extends React.Component {
+class AmazingCropperPage extends React.Component {
+  onDone = (croppedImageUri) => {
+    console.log('croppedImageUri = ', croppedImageUri);
+    // send image to server for example
+  }
+
+  onCancel = () => {
+    console.log('Cancel button was pressed');
+    // navigate back
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Tab1 Screen1</Text>
-        <Button title="Navigate to Tab1Screen2" onPress={() => { this.props.navigation.navigate("Tab1Screen2"); }} />
-      </View>
+      <AmazingCropper
+        onDone={this.onDone}
+        onCancel={this.onCancel}
+        imageUri='https://www.lifeofpix.com/wp-content/uploads/2018/09/manhattan_-11-1600x2396.jpg'
+        imageWidth={1600}
+        imageHeight={2396}
+        NOT_SELECTED_AREA_OPACITY={0.3}
+        BORDER_WIDTH={20}
+      />
     );
   }
 }
 
-class Tab1Screen2Component extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Tab1 Screen2</Text>
-      </View>
-    );
-  }
+function CenteredText({ text, children }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{text}</Text>
+      {children}
+    </View>
+  );
 }
 
-const Tab1Stack = createStackNavigator();
-
-class Tab1StackComponent extends React.Component {
-  render() {
-    return (
-      <Tab1Stack.Navigator>
-        <Tab1Stack.Screen
-          name="Tab1Screen1"
-          component={Tab1Screen1Component}
-        />
-        <Tab1Stack.Screen name="Tab1Screen2" component={Tab1Screen2Component} />
-      </Tab1Stack.Navigator>
-    );
-  }
+function Tab1Component() {
+  return <AmazingCropperPage />;
 }
 
-class Tab2Component extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Tab2</Text>
-      </View>
-    );
-  }
+function Tab2Component() {
+  return <CenteredText text="Tab2" />;
 }
 
 const Tabs = createBottomTabNavigator();
@@ -60,7 +57,7 @@ class TabsComponent extends React.Component {
       <Tabs.Navigator>
         <Tabs.Screen
           name="Tab1"
-          component={Tab1StackComponent}
+          component={Tab1Component}
           options={{
             tabBarIcon: ({focused, color, size}) => {
               return <FontAwesome name="star-o" size={size} color={color} />;
@@ -81,10 +78,50 @@ class TabsComponent extends React.Component {
   }
 }
 
+function FirstStackScreen() {
+  return <AmazingCropperPage />;
+}
+
+function SecondStackScreen() {
+  return <TabsComponent />;
+}
+
+const MainStack = createStackNavigator();
+
+function MainStackComponent() {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen
+        name="FirstStackScreen"
+        component={FirstStackScreen}
+        options={({ navigation, route }) => ({
+          headerRight: ({ tintColor }) => {
+            return (
+              <FontAwesome
+                name="arrow-right"
+                size={24}
+                color={tintColor}
+                style={{ paddingRight: 25 }}
+                onPress={() => {
+                  navigation.navigate("SecondStackScreen");
+                }}
+              />
+            );
+          }
+        })}
+      />
+      <MainStack.Screen
+        name="SecondStackScreen"
+        component={SecondStackScreen}
+      />
+    </MainStack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <TabsComponent />
+      <MainStackComponent />
     </NavigationContainer>
   );
 }
